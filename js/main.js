d8d6911e63ae60bff5dbd7a53f9dd2195d512a26
Gonza <gtorres@3dglabs.com.ar>
2019-08-20 16:32:14 -0300

    Agrego menu mobile

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
-$(function(){
+$(function() {
   $('.home-slider, .owl-carousel').owlCarousel({
-    loop:true,
+    loop: true,
     autoplay: false,
-    margin:0,
-    nav:false,
+    margin: 0,
+    nav: false,
     autoplayHoverPause: false,
     items: 1,
-    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1
+    navText: [
+      "<span class='ion-md-arrow-back'></span>",
+      "<span class='ion-chevron-right'></span>"
+    ],
+    responsive: {
+      0: {
+        items: 1
       },
-      600:{
-        items:1
+      600: {
+        items: 1
       },
-      1000:{
-        items:1
+      1000: {
+        items: 1
       }
     }
   });
-})
+
+  $('#menu-trigger').click(function() {
+    $('nav').addClass('show-menu');
+  });
+  $('#close-menu').click(function() {
+    $('nav').removeClass('show-menu');
+  });
+});
